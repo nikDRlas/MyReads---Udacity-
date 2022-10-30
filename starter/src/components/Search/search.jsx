20cd088d31d8cc42a35a52a6e83219a72527521a
Niklas Lindenbaum <niklas.lindenbaum@t-online.de>
2022-10-31 00:49:12 +0100

    test2

---
+++ starter/src/components/Search/search.jsx
@@ -0,0 +1,78 @@
+import Navbar from "../layout/Navbar/navhbar";
+import {useEffect, useState} from "react"
+import { search } from "../../BooksAPI";
+import Book from "../Books/Book";
+import "./search.css"
+import { Link } from "react-router-dom"
+
+
+const SearchPage = ({showSearchPage, setShowSearchpage, changeShelf, shelf, books}) => {
+    const [query, setQuery] = useState();
+    const [searchedBooks, setSearchedBooks] = useState([])
+    const [searchMessage, setSearchMessage] = useState(false)
+    const matchingBooks = [];
+
+
+    const getSearch = async(query) => {
+    
+        if(!query){
+            setSearchedBooks(null);
+            return;
+        }
+        const searchResults = await search(query);
+        console.log(searchResults);
+        if(searchResults.error){
+            setSearchedBooks([])
+            setSearchMessage(true)
+            return;
+        }
+        setSearchedBooks(searchResults);
+         setSearchMessage(false)
+    }
+
+    useEffect(()=> {
+        getSearch(query);
+    }, [query])
+
+    // eslint-disable-next-line no-lone-blocks
+    searchedBooks && searchedBooks.map(searchResult => {
+        const matchingResults = books.filter(book => book.id === searchResult.id);
+
+        if (matchingResults.length > 0)
+        {
+
+            return matchingBooks.push(...matchingResults);
+
+        } 
+
+        return matchingBooks.push(searchResult)
+
+        
+    });
+
+console.log();
+    return ( 
+        <>
+        <div className="search-books">
+          <div className="search-books-bar">
+             <Link className="close-search" to="/">Home</Link>
+            <div className="search-books-input-wrapper">
+              <input
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+                type="text"
+                placeholder="Search by title, author, or ISBN"
+              />
+            </div>
+          </div> 
+          <div className="search-books-results">
+            <ol className="books-grid"></ol>
+           { searchMessage &&  <p className="no-result-message">No books found. Please search for another book!</p> }
+            {searchedBooks &&  <Book books={matchingBooks} changeShelf={changeShelf} shelf={shelf} select={"none"}/>}
+          </div>
+        </div>
+        </>
+     );
+}
+ 
+export default SearchPage;
---
+++ starter/src/components/layout/Navbar/navhbar.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import "./navbar.css"
+import { Link } from "react-router-dom"
+const Navbar = ({setShowSearchpage, showSearchPage}) => {
+
+
+
+
+    return (  
+        <div className="list-books">
+          <div className="list-books-title">
+            <h1>My Reading List</h1>
+            <div className="nav">
+                <Link className="link home-link"to="/">Home</Link>
+                <Link to="/search" className="link search-link">Search</Link>
+            </div>
+             </div>
+            
+          
+          </div>
+
+    );
+}
+ 
+export default Navbar;
